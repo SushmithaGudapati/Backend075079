4b428f021c8b7f3d7423ae7ae1c6b66ea2c2d3a4
Sushmithagudapati <gudapatisushmitha111@gmail.com>
2023-09-16 15:19:44 +0530

    first commit

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:"mongodb+srv://SushmithaGudapati:Sushmitha2293@cluster0.p3rl1je.mongodb.net/mern-hotel?retryWrites=true&w=majority",
+	secretOrKey: "eTUbHZ4yhlPlC8ioP35Ttbwa0YwbOw3vsuTuo3JsT3k4Mp1avHWLNMsPlvJBitsr"
+}; 
