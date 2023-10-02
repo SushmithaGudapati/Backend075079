a2fa303258bbf8bccc6aae94b70cdfb9adc6cedf
SushmithaGudapati <gudapatisushmitha111@gmail.com>
2023-10-02 18:46:09 +0530

    backend

--- client/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-white mt-5 pd-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} <a href="https://github.com/starkblaze01/Hostel-Management#team-members">Team Web Tech</a>
-    </footer>
-  );
-}
